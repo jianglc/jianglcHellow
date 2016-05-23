331da3110fcb6f03de86e6208d61b0d1f69386a0
jianglc <39652459@qq.com>
2016-05-23 18:25:09 +0800

    test
    dddd

---
+++ www/cordovaHelloword.js
@@ -0,0 +1,33 @@
+﻿/**
+ * cordova plugin
+ * Copyright (c) ljx
+ *
+ */
+ (function(cordova){
+    var cordovaHelloword = function() {
+
+    };  
+
+    cordovaHelloword.prototype.startConlose = function (success, fail) {
+        return cordova.exec(function (args) {
+            success(args);
+        }, function (args) {
+            fail(args);
+        }, 'CordovaAlter', 'startConlose', []);
+    };
+
+
+    cordovaHelloword.prototype.startActivity = function (params, success, fail) {
+        return cordova.exec(function(args) {
+            success(args);
+        }, function(args) {
+            fail(args);
+        }, 'cordovaHelloword', 'startActivity', [params]);
+    };
+ 
+    window.cordovaAlter = new cordovaHelloword();
+    
+    // backwards compatibility
+    window.plugins = window.plugins || {};
+    window.plugins.cordovaAlter = window.cordovaAlter;
+})(window.PhoneGap || window.Cordova || window.cordova);
